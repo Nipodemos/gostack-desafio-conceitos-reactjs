26e2be5bb772a315d479dff7b4ce7ef5ac1e0958
Nipodemos <nipodemos13@gmail.com>
2020-06-20 13:35:46 -0300

    desafio completo!!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then((response) => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    let mockData = {
+      "title": "Flexbox na prática" + Date.now().toString(),
+      "url": "http://github.com/nipodemos/flexbox-na-prática",
+      "techs": [
+        "CSS Flexbox"
+      ],
+    }
+
+    api.post('/repositories', mockData).then((response) => {
+      const createdRepository = response.data
+      setRepositories([...repositories, createdRepository])
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    api.delete(`/repositories/${id}`).then((response) => {
+      console.log('response :>> ', response);
+      if (response.status === 204) {
+
+        let newRepositories = repositories.filter((value) => {
+          return (value.id !== id)
+        })
+        console.log('newRepositories :>> ', newRepositories);
+
+        setRepositories(newRepositories)
+      }
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+                </button>
+            </li>
+          )
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
